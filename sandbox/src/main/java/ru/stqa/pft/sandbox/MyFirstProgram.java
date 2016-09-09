debc6790eb06e359722d642be9efe9b9cb627b52
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-09 16:44:06 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
 
+    double size = 5;
+    System.out.println("Площадь квадрата со стороной " + size + " = " + area(size));
+
+    double a = 4;
+    double b = 2;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+    }
+
+  public static void hello (String somebody){
+    System.out.println("Hello, " + somebody);
+  }
+
+  public static double area (double len){
+    return len * len;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
   }
 }
