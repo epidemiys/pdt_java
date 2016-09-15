500e5cb17b7ba473734b53a7f3288d8a7795ca8a
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-15 23:02:51 +0300

    dz3

---
+++ sandbox/src/test/java/dz/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,34 @@
+package dz.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by aleksandr.petrov on 15.09.16.
+ */
+public class PointTest {
+
+  @Test
+  //Проверка расстояния равного нулю
+  public void testNull(){
+    Point a = new Point(0, 0);
+    Point b = new Point(0, 0);
+    Assert.assertEquals(a.distance(b) , 0.0);
+  }
+
+  @Test
+  //Проверка расстояния не равного нулю
+  public void testNonNull(){
+    Point a = new Point(1, 1);
+    Point b = new Point(2, 2);
+    Assert.assertEquals(a.distance(b) , 1.4142135623730951);
+  }
+
+  @Test
+  //Проверка того, что значение не равно корректному. Проверка бред, просто потрогал новый метод.
+  public void testPo(){
+    Point a = new Point(1, 1);
+    Point b = new Point(2, 2);
+    Assert.assertNotEquals(a.distance(b) , 0);
+  }
+}
