e93a2e859a72c27f5b88f5ce3015ef58a7240701
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-18 00:17:42 +0300

    fix

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHealper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHealper.java
@@ -6,20 +6,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by aleksandr.petrov on 17.09.16.
  */
-public class SessionHealper {
-    private FirefoxDriver wd;
+public class SessionHealper extends HelperBase {
 
     public SessionHealper(FirefoxDriver wd) {
-        this.wd = wd;
+            super(wd);
     }
 
     public void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//form[@id='LoginForm']/input[3]"));
     }
 }
