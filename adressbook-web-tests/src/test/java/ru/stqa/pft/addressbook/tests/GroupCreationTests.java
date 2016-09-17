a81c838a8f31813fefebc7ed4034c36e75eba8e1
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-17 18:37:29 +0300

    fix

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void testGroupCreation() {
+        app.goToGroupPage();
+        app.initGroupCreation();
+        app.fillGroupForm(new GroupData("dfdfdf", "cxcx", "scxcxc"));
+        app.submitGroupCreation();
+        app.returnToGroupPage();
+    }
+}
