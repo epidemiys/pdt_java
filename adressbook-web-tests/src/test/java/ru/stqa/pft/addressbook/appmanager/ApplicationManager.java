e0cec80e5b5f412df256613ca10f7bd738796f06
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-22 22:32:52 +0300

    Добавил работу с новыми браузерами

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,11 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,15 +14,29 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHealper sessionHealper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+            if (browser.equals(BrowserType.FIREFOX)){
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)){
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.SAFARI)) {
+                wd = new SafariDriver();
+            } else if (browser.equals(BrowserType.OPERA)) {
+                wd = new OperaDriver();
+            }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
