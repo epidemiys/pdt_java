e47312a5314b2e9ae6986ea4db3795593e9e8fd8
aleksandr.petrov <aleksandr.petrov@corp.mail.ru>
2016-10-23 00:15:59 +0300

    повторил 7 урок 2 задание

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectiobTest.java
@@ -0,0 +1,47 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
+
+/**
+ * Created by aleksandr.petrov on 22.10.16.
+ */
+public class HbConnectiobTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+
+    protected void setUp() throws Exception {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    @Test
+    public void hbConnectionTest(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery("from GroupData").list();
+        for (GroupData group : result) {
+            System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+}
