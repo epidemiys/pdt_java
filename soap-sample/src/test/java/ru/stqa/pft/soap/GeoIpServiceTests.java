94ae6b2d178207ed90dd2e3af7bed333d617c66b
aleksandr.petrov <aleksandr.petrov@corp.mail.ru>
2016-11-09 23:34:31 +0300

    Повторил 9 урок 2 модуль

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by aleksandr.petrov on 09.11.16.
+ */
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp(){
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("46.39.248.130");
+        assertEquals(geoIP.getCountryCode(), "RUS");
+    }
+}
