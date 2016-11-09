9c2f0f88563781381e9335ebceaea1029133d540
aleksandr.petrov <aleksandr.petrov@corp.mail.ru>
2016-11-09 23:55:15 +0300

    Повторил 9 занятие 3тий модуль

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.mantis.tests;
+
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import org.testng.annotations.Test;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+
+/**
+ * Created by aleksandr.petrov on 09.11.16.
+ */
+public class SoapTests {
+
+    @Test
+
+    public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mc = new MantisConnectLocator()
+                .getMantisConnectPort(new URL("http://localhost/mantisbt-1.3.3/api/soap/mantisconnect.php"));
+        ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+        System.out.println(projects.length);
+        for (ProjectData project: projects){
+            System.out.println(project.getName());
+        }
+
+
+    }
+}
