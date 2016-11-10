a9ad8cfdf82eb376b1fa18cdef7670b950d200a7
aleksandr.petrov <aleksandr.petrov@corp.mail.ru>
2016-11-10 20:07:18 +0300

    Повторил 9ый урок 4ый модуль

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,16 +1,11 @@
 package ru.stqa.pft.mantis.tests;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.Set;
 
@@ -43,5 +38,4 @@ public class SoapTests extends TestBase{
         Issue created = app.soap().addIssue(issue);
         assertEquals(issue.getSummary(), created.getSummary());
     }
-
 }
